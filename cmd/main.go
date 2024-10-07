e015749aabbcce151d73f122cb058d3d965e0ad7
serafimcode <serafim.code@gmail.com>
2024-10-07 21:14:34 +0700

    feat/39509481: added config to parse .env and connected to DB

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"reliab-test/internal/config"
+	"reliab-test/internal/infrastructure/datastore"
+	"reliab-test/internal/infrastructure/log"
+	"reliab-test/internal/repositories/users_repository"
 )
 
 func main() {
+	defaultLog := log.InitDefaultLogger()
+
+	cfg, err := config.Load(defaultLog)
+	if err != nil {
+		defaultLog.Error("Terminate execution", err)
+	}
+
+	logger := log.InitLogger()
+
+	db := datastore.InitDB(cfg.DB, logger)
+
+	userRepository := users_repository.BuildUserRepository(db)
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
+		})
+	})
+	r.GET("/users", func(c *gin.Context) {
+		users, err := userRepository.GetUsers(c.Request.Context())
+		if err != nil {
+			logger.Error("Failed to get users", err)
+			c.JSON(http.StatusInternalServerError, err)
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"data": users,
 		})
 	})
 	r.Run()
